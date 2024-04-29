b2acea0d9e6230ecba5a754d4ffdb3efb38606db
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-04-29 20:32:47 +0530

    Fixed OrganizationSwitcher not reflecting url changes

--- app/(platform)/(dashboard)/_components/CustomOrgSwitcher.tsx
+++ app/(platform)/(dashboard)/_components/CustomOrgSwitcher.tsx
@@ -2,12 +2,14 @@
 
 // for checking the current theme and returning the org Switcher component according to theme
 import { useEffect, useState } from "react";
-import { OrganizationSwitcher } from "@clerk/nextjs";
+import { OrganizationSwitcher, useOrganizationList } from "@clerk/nextjs";
 import { Skeleton } from "@/components/ui/skeleton";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
-
+import { useParams } from "next/navigation";
 const CustomOrgSwitcher = () => {
+  const params = useParams();
+  const { setActive } = useOrganizationList();
   const { resolvedTheme } = useTheme();
   const [loading, setLoading] = useState(true); // Add loading state
   //   to avoid hydration warning
@@ -18,6 +20,14 @@ const CustomOrgSwitcher = () => {
     return () => clearTimeout(timer);
   }, []);
 
+  // to set active org based on the url params
+  useEffect(() => {
+    if (!setActive) return;
+    // setting the active org based on the url params everytime id is changed
+    setActive({
+      organization: params.id as string,
+    });
+  }, [setActive, params.id]);
   return (
     <>
       {loading ? (
