8ab01711c4f0414d9dac7bd4f00f1de00f1e62ba
ayushvyas-1 <ayushkv360@gmail.com>
2024-05-01 12:21:48 +0530

    Implement Sidebar Component

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -4,11 +4,14 @@ import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import CustomOrgSwitcher from "./CustomOrgSwitcher";
 import CustomUserButton from "./CustomUser";
+import { MobileSidebar } from "./mobile-sidebar";
 
 const DashBoardNavbar = () => {
   return (
     <nav className="fixed left-0 top-0 z-50 h-16 w-full border-b bg-white shadow-sm dark:bg-app-dark-navbar">
       <div className="flex h-full w-full items-center px-2 sm:px-4 md:px-6">
+      <MobileSidebar/>
+
         {/* this div is for displaying logo and create button */}
         <div className="flex items-center space-x-3">
           <Image
---
+++ hooks/use-mobile-sidebar.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+//Define type of hook to be created
+type MobileSidebarStore = {
+    isOpen:boolean;
+    onOpen:()=>void;
+    onClose:()=>void;
+};
+
+
+//create a hook with these properties
+export const useMobileSidebar = create<MobileSidebarStore>((set)=>({
+    isOpen:false,
+    onOpen:()=>set({isOpen:true}),
+    onClose: ()=>set({isOpen:false}),
+}))
